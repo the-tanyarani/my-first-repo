9b5e6c16d74bfddae3fd91fe8d1fd8f09cf1770a
the-tanyarani <tanyarani.data@gmail.com>
2025-09-08 15:57:06 +0530

    Add files via upload

---
+++ Q1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,avg;
+    cout<<" enter the value of a, b and c"<<endl<< "a = ";
+    cin>>a;
+    cout<<"b = ";
+    cin>>b;
+    cout<<"c = ";
+    cin>>c;
+    avg=(a+b+c)/3;
+    cout<<"The average of a, b and c is "<<avg;
+    return 0;
+}
